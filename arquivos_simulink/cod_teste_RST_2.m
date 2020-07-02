9923071a3c200d9b5f5b83f1fcc1b32bb3262e73
Tarcísio Lima Ferreira <tlf@ic.ufal.br>
2020-07-02 19:46:17 -0300

    Método de alocação de polos atualizado
    Fui utilizado o método descrito no livro Digital Control Systems, Design, Identification, and Implementation (Landau e Zito). O RST com MQR no simulink está com problemas quando ocorrer pertubações no sistema

--- arquivos_simulink/cod_teste_RST_2.m
+++ arquivos_simulink/cod_teste_RST_2.m
@@ -12,9 +12,13 @@ sys = filt(num,den, T_a);
 A = den;
 B = num;
 
+% degA = polynomialDegree(polyA);
+% degB = polynomialDegree(polyB);
+
 Ts=1;                      %Tempo de estabelecimento desejado malha fechada
 ep=0.6;                     %Epsilon (Coeficiente de amortecimento)
 wn=4/(ep*Ts);               %Frequencia natural do sistema
+Mp = exp((-ep*pi)/sqrt(1 - ep^2));% Overshoot 
 
 %Polos conjugados
 t1 = -2*exp(-ep*wn*T_a);
@@ -30,11 +34,32 @@ p2=exp(-2*ep*wn*T_a);
 Am=[1 p1 p2 0];
  
 %Resolução de Equações metodo matricial
-M=[1 0 B(2); A(2) B(2) B(3); A(3) B(3) 0];
-q=[Am(2)-A(2); Am(3)-A(3);Am(4)];
-X=inv(M)*q;
- 
-R=[1 X(1)];             %Polinomio R
-S=[X(3) X(2)];            %Polinomio S
+%M=[1 0 B(2); A(2) B(2) B(3); A(3) B(3) 0];
+
+%q=[Am(2)-A(2); Am(3)-A(3);0;0];
+
+
+M = [1 0 0 0;
+    A(2) 1 B(2) 0;
+    A(3) A(2) B(3) B(2);
+    0 A(3) 0 B(3)];
+
+p = [1; p1; p2; 0];
+
+X=inv(M)*p;
+
+Na = 2;
+Nb = 1;
+d = 1;
+
+Nr = Na - 1;
+Ns = Nb +d -1;
+
+%R=[1 X(1)];             %Polinomio R
+%S=[X(3) X(2)];            %Polinomio S
+
+R = [X(1) X(2)];
+S = [X(3) X(4)];
 %Polinomio T
 T=sum(Am)/sum(B);
+
