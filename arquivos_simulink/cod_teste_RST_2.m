934b8734b23773d3527021923b1cf878aff7e336
Tarcísio Lima Ferreira <tlf@ic.ufal.br>
2020-07-03 12:08:19 -0300

    Código do RST atualizado para mostrar a FT em TD desejada

--- arquivos_simulink/cod_teste_RST_2.m
+++ arquivos_simulink/cod_teste_RST_2.m
@@ -1,14 +1,16 @@
 clear
 clc
 %% Planta
-Ta=0.1;                          %Tempo de amostra
+Ta=0.1;                         %Tempo de amostra
 num=1;                          %Numerador Continuo
 den=[0.1 1.1 1];                %Denominador Continuo
 gp=tf(num,den);                 %Função de transferencia
-ftz=c2d(gp,Ta, 'zoh');           %Planta Discreta
+ftz=c2d(gp,Ta, 'zoh');          %Planta Discreta
 
-[num,den] = tfdata(ftz, 'v')         %num e den discreto
+[num,den] = tfdata(ftz, 'v');         %num e den discreto
 sys = filt(num,den, Ta);
+sys_d = set(sys, 'variable', 'z^-1'); %Função de Transferência em TD
+
 A = den;
 B = num;
 
@@ -22,10 +24,19 @@ Ns = Nb +d -1;
 %Condições do polinômio P(z^-1)
 %0.25 <= w0Ts <= 1.5 ; 0.7 <= zeta <= 1
 
-Ts=1;                      %Tempo de estabelecimento desejado malha fechada
-ep=0.6;                     %Epsilon (Coeficiente de amortecimento)
-wn=4/(ep*Ts);               %Frequencia natural do sistema
-Mp = exp((-ep*pi)/sqrt(1 - ep^2));% Overshoot 
+Ts=3.9;                     %Tempo de estabelecimento desejado malha fechada
+ep=0.7;                            %Epsilon (Coeficiente de amortecimento)
+wn=4/(ep*Ts);                      %Frequencia natural do sistema
+Mp = exp((-ep*pi)/sqrt(1 - ep^2)); %Overshoot 
+
+%Função de Transferência em Malha Fechada desejada
+z = ep;
+[numd,dend]=ord2(wn,z);
+
+gpd = tf(numd,dend);
+ftzd= c2d(gpd,Ta, 'zoh');           %Planta Discreta
+sysd = filt(numd,dend, Ta);
+tfd = set(sysd, 'variable', 'z^-1'); %Função de Transferência em TD
 
 %Polos conjugados
 t1 = -2*exp(-ep*wn*Ta);
@@ -33,11 +44,13 @@ t2 = sqrt(1-ep^2);
 t3 = wn*Ta;
 t4 = cos(wn*Ta*sqrt(1-ep^2));
 
-
+%Coeficientes do polinomio desejado de acordo 
+%com as especificações de desempenho
 p1=-2*exp(-ep*wn*Ta)*cos(wn*Ta*sqrt(1-ep^2));
 p2=exp(-2*ep*wn*Ta);
 
-% Polinomio desejado com um polo insignificante igual a zero
+%Coeficientes do polinomio desejado 
+%com um polo insignificante igual a zero
 Am=[1 p1 p2 0];
 
 %Resolução de Equações metodo matricial
@@ -53,7 +66,7 @@ p = [1; p1; p2; 0];
 
 X=inv(M)*p;
 
-% Polinomio R e S
+%Polinomio R e S
 R = [X(1) X(2)];
 S = [X(3) X(4)];
 
